6c11d52118067961ead85d2922862ff20022bf22
Andrew Mweene <mweeneandrew8@gmail.com>
2025-12-30 16:11:47 +0200

    MERN Stack App - Update API URL in frontend

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const mongoose = require('mongoose')
 const express = require('express')
+const cors = require('cors')
 const workoutRoutes = require('./routes/workouts')
 
 //express app
@@ -9,6 +10,8 @@ const app  = express()
 
 //middleware
 app.use(express.json())
+
+app.use(cors())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
