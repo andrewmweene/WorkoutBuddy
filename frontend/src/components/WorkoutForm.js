e51ef9ece8a65c08042ab35ff4e701ebd7e4af9c
Andrew Mweene <mweeneandrew8@gmail.com>
2025-12-30 13:03:04 +0200

    Initial Commit

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { useWorkoutContext } from "../hooks/useWorkoutContext";
+
+const WorkoutForm = () => {
+    const  { dispatch }  = useWorkoutContext();
+    const [title, setTitle] = useState('');
+    const [load, setLoad] = useState('');
+    const [reps, setReps] = useState('');
+    const [error, setError] = useState('null');
+    const [emptyFields, setEmptyFields] = useState([]);
+
+    const handleSubmit = async (e)  => {
+        e.preventDefault()
+
+        const workout = {title, load, reps}
+
+        const response = await fetch('/api/workouts', {
+            method:'POST',
+            body: JSON.stringify(workout),
+            headers: {
+                'content-Type': 'application/json'
+            }
+        })
+        const json = await response.json()
+        if (!response.ok){
+            setError(json.error);
+            setEmptyFields(json.emptyFields);
+        }
+        if (response.ok) {
+            setTitle('');
+            setLoad('');
+            setReps('');
+            setError(null);
+            setEmptyFields([]);
+            console.log('New workout added');
+            dispatch({type: 'CREATE_WORKOUT', payload: json});
+        }
+    }
+
+    return (
+        <form className="create" onSubmit={handleSubmit}>
+            <h3>Add a new workout</h3>
+
+            <label>Exercise title</label>
+            <input 
+                type="text"
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+                className={emptyFields.includes('title') ? 'error': ''}
+                />
+            
+            <label>Load (kg)</label>
+            <input
+                type="text"
+                onChange={(e) => setLoad(e.target.value)}
+                value={load}
+                className={emptyFields.includes('load') ? 'error': ''}
+                />
+            <label>Reps</label>    
+            <input 
+                type="text"
+                onChange={(e) => setReps(e.target.value)}
+                value={reps}
+                className={emptyFields.includes('reps') ? 'error': ''}
+            />
+            <button>Add workout</button>
+            {error && <div  className="error">{error}</div>}
+        </form>
+    )
+}
+export default WorkoutForm;
